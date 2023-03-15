e8f20d1f9290e91f61783d041da810cb956d9cd5
Stusowski <126696159+Stusowski@users.noreply.github.com>
2023-03-15 15:44:02 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+   int i,w=0;
+    for(i=0;i<101;i++)
+      {
+        if(i%2==0)
+        {
+          w=w+i;
+        }
+      }
+    
+      System.out.println(w);
+  }
+}
